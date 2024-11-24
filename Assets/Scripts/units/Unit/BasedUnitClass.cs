196be85252da8cdf5227905a4a0e4a2b54892974
fug683 <romananishenko12345@gmail.com>
2024-11-24 17:12:21 +0700

    update units

--- Assets/Scripts/units/Unit/BasedUnitClass.cs
+++ Assets/Scripts/units/Unit/BasedUnitClass.cs
@@ -3,8 +3,46 @@ using UnityEngine;
 public class BasedUnitClass : MonoBehaviour
 {
 
+    protected struct Characteristics {
+
+        private int HP;
+        private int AR;
+        private int EN;
+        private int SP;
+        private int VR;
+        private int AT;
+
+        public Characteristics(int hp, int ar, int en, int sp,int vr,int at)
+        {
+            this.HP = hp;
+            this.AR = ar;
+            this.EN = en;
+            this.SP = sp;
+            this.VR = vr;
+            this.AT = at;
+        }
+
+        public int[] get() {
+            return new int[] {this.HP, this.AR, this.EN, this.SP, this.VR, this.AT};
+        }
+
+        public void Damage(int dmg) {
+            this.HP -= dmg;
+        }
+        public void Change_EN(int en) {
+            this.EN -= en;
+        }
+
+    }
 
 
+
+    public BasedUnitClass() {
+    
+        
+
+    
+    }
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,8 +61,8 @@ public class BasedUnitClass : MonoBehaviour
     
     
     }
-    void Spawn(Vector3 poz) { 
-    
+    void Spawn(Vector3 poz) {
+        transform.position = poz;
     
     }
 
