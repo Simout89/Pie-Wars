57abe7e084e10ba0f8be8f8355d7561f93004178
Albert <alik2006sootkill@gmail.com>
2024-11-30 18:22:07 +0700

    Update

--- Assets/_Script/Sound/AudioSourceController.cs
+++ Assets/_Script/Sound/AudioSourceController.cs
@@ -5,19 +5,33 @@ using Zenject;
 public class AudioSourceController : MonoBehaviour
 {
     [Inject] SoundManager _soundManager;
-    private AudioSource audioSource;
+    private AudioSource _audioSource;
+    private float _globalVolume = 1;
+    private float _volume = 1;
     
     [SerializeField] private SoundType _soundType;
 
     private void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
+        _audioSource = GetComponent<AudioSource>();
         _soundManager.volumeChanged += HandleVolumeChanged;
     }
 
     private void HandleVolumeChanged(float volume, SoundType soundType)
     {
+        if(!(soundType == _soundType || soundType == SoundType.Global))
+            return;
+        
         if(soundType == _soundType)
-            audioSource.volume = volume;
+        {
+            _volume = volume;
+        }
+        else if (soundType == SoundType.Global)
+        {
+            _globalVolume = volume;
+        }
+        _audioSource.volume = _volume * _globalVolume;
+        
+        Debug.Log($"SoundType: {_soundType} Глобал: {_globalVolume} Громокость: {_volume}");
     }
 }
