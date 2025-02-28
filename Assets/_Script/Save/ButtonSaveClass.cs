a5134c5e1c23b2c9275e722630bac5b224cc0d8d
Albert <alik2006sootkill@gmail.com>
2025-02-28 17:29:48 +0700

    rebindsave0.1

--- Assets/_Script/Save/ButtonSaveClass.cs
+++ Assets/_Script/Save/ButtonSaveClass.cs
@@ -1,10 +1,60 @@
-using System;
-using Unity.VisualScripting;
+using System.IO;
+using Newtonsoft.Json; // Используем Newtonsoft.Json для форматирования
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-
-[Serializable]
-public class ButtonSaveClass
+public class ControlSettingsManager : MonoBehaviour
 {
-    private string _name;
+    private string SettingsFilePath
+    {
+        get
+        {
+            // Путь к папке Application Support
+            string folderPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData), "../Application Support/YourGame");
+            folderPath = Path.GetFullPath(folderPath); // Преобразование в абсолютный путь
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            return Path.Combine(folderPath, "controlSettings.json");
+        }
+    }
+
+    public InputActionAsset inputActions;
+
+    public void SaveSettings()
+    {
+        // Получаем JSON с привязками
+        var bindings = inputActions.SaveBindingOverridesAsJson();
+
+        // Форматируем JSON для читаемости
+        var jsonObject = JsonConvert.DeserializeObject<object>(bindings);
+        var formattedJson = JsonConvert.SerializeObject(jsonObject, Formatting.Indented); // Formatting.Indented для читаемого формата
+
+        // Сохраняем в файл
+        File.WriteAllText(SettingsFilePath, formattedJson);
+        Debug.Log($"Control settings saved to {SettingsFilePath}");
+    }
+
+    public void LoadSettings()
+    {
+        if (File.Exists(SettingsFilePath))
+        {
+            // Читаем привязки из файла
+            var bindings = File.ReadAllText(SettingsFilePath);
+            inputActions.LoadBindingOverridesFromJson(bindings);
+            Debug.Log("Control settings loaded.");
+        }
+        else
+        {
+            Debug.LogWarning("No control settings file found, using defaults.");
+        }
+    }
+
+    public void ResetSettings()
+    {
+        // Сбрасываем все пользовательские привязки
+        inputActions.RemoveAllBindingOverrides();
+        Debug.Log("Control settings reset to defaults.");
+    }
 }
