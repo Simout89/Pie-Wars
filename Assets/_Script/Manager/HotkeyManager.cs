c0834db9371a741efd08452795f5f04ee27e9655
Albert <alik2006sootkill@gmail.com>
2025-02-28 16:22:52 +0700

    rebind3

--- Assets/_Script/Manager/HotkeyManager.cs
+++ Assets/_Script/Manager/HotkeyManager.cs
@@ -5,14 +5,21 @@ public class HotkeyManager : MonoBehaviour
 {
     [SerializeField] private InputActionAsset controls;
     private InputAction saveAction;
+    private InputAction nextAction;
 
     void Awake()
     {
         var gameplay = controls.FindActionMap("Player");
         saveAction = gameplay.FindAction("Save");
+        nextAction = gameplay.FindAction("Next");
 
+        nextAction.performed += _ =>
+        {
+            Debug.Log("Next");
+        };
         saveAction.performed += _ => SaveGame();
         saveAction.Enable();
+        nextAction.Enable();
     }
 
     void SaveGame()
