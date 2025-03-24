f05662c074062a2d406ac58b9fd59882c3814890
Benaiv <62600634+Benaiv@users.noreply.github.com>
2025-03-24 21:08:55 +0300

    add Hex grid for building system
    change color

--- Assets/_Script/HexGrid/HexCell.cs
+++ Assets/_Script/HexGrid/HexCell.cs
@@ -1,16 +1,78 @@
+using System;
 using UnityEngine;
+
+
 
 public class HexCell : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    public int elevation;
+    public HexCoordinates coordinates;
+
+    
+    void Awake()
     {
-        
+        MakeCHex();
     }
 
-    // Update is called once per frame
+
+
+
+
+
+
+    private void MakeCHex()   //создание гекса
+    {
+        Vector3 Center = transform.InverseTransformPoint(transform.position);
+        Console.Write(Center);
+        //Console.WriteLine("1234421");
+        //Console.WriteLine(Center.x);
+        MeshCollider collider = GetComponent<MeshCollider>();
+        MeshFilter Mesh_Filter = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        Mesh_Filter.mesh = mesh;
+        mesh.Clear();
+
+        Vector3[] vert = new Vector3[8] {
+            Center, new Vector3(), new Vector3() ,new Vector3() ,new Vector3(),new Vector3(),new Vector3(),new Vector3(),
+        };
+        
+        
+        int[] tri = new int[18];
+
+        tri[0] = 0;     tri[9] = 0;
+        tri[1] = 1;     tri[10] = 4;
+        tri[2] = 2;     tri[11] = 5;
+
+        tri[3] = 0;     tri[12] = 0;
+        tri[4] = 2;     tri[13] = 5;
+        tri[5] = 3;     tri[14] = 6;
+
+        tri[6] = 0;     tri[15] = 0;
+        tri[7] = 3;     tri[16] = 6;
+        tri[8] = 4;     tri[17] = 1;
+      
+
+        for (int i = 1; i < 7; i++) {
+            vert[i] = Center+HexMetrics.corners[i];
+        }
+
+
+        
+
+        mesh.vertices = vert;
+        mesh.triangles = tri;
+        Console.WriteLine(Center);
+        //collider.sharedMesh = mesh;
+
+    }
+
     void Update()
     {
-        
+       
     }
+
+
 }
+
